bc997a4cc886f10463cf01c230d0f85987b307de
Chris4mny <79114581+Chris4mny@users.noreply.github.com>
2021-06-29 17:34:43 -0400

    Add files via upload

---
+++ FibonacciSequence/FibonacciSequence/Fibonacci.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciSequence
+{
+    class Fibonacci
+    {
+        private int firstNumber = 0;
+        private int SecondNumber = 1;
+        private int nextNumber;
+        private int numberOfElements;
+
+        public Fibonacci() // constructor
+        {
+            FibonacciSequence();
+        }
+
+        public void FibonacciSequence()
+        {
+            Console.Write("Enter the number of elements to Print: **Number be greater than 2**: ");
+            numberOfElements = int.Parse(Console.ReadLine());
+
+            if (numberOfElements < 2)
+            {
+                Console.WriteLine("Please enter a number greater than two");
+            }
+            else
+            {
+                //First print first and second number
+                Console.Write(firstNumber + " " + SecondNumber + " ");
+
+                //Starts the loop from 2 because 0 and 1 are already printed
+                for (int i = 2; i < numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + SecondNumber;
+                    Console.Write(nextNumber + " ");
+                    firstNumber = SecondNumber;
+                    SecondNumber = nextNumber;
+                }
+            }
+        }
+    }
+}
